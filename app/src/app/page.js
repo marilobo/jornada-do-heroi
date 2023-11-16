ce83fe64ebd54b8ecd8a12de4188528de415f33d
Mariana <mlobosilva@outlook.com>
2023-11-16 15:12:01 -0300

    tests

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -11,41 +11,46 @@ export default async function Topster() {
     <div>
       <h1>Heróis</h1>
       {/* <pre>{JSON.stringify(herois,null,2)}</pre> */}
-      <List>
-        <ListItem>
-          <ListItemText primary="Id:" secondary={herois[0].id}></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Name:" secondary={herois[0].name}></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Slug:" secondary={herois[0].slug}></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Powerstats:">
-          </ListItemText>          
-          <List component="div">
+      {
+        herois.map((heroi) => (
+          <List key={heroi.id}>
+            <img src={heroi.images.lg} alt="batata" width={150} />
             <ListItem>
-              <ListItemText primary="Intelligence:" secondary={herois[0].powerstats.intelligence}></ListItemText>
+              <ListItemText primary="Id:" secondary={heroi.id}></ListItemText>
             </ListItem>
             <ListItem>
-              <ListItemText primary="Strength:" secondary={herois[0].powerstats.strength}></ListItemText>
+              <ListItemText primary="Name:" secondary={heroi.name}></ListItemText>
             </ListItem>
             <ListItem>
-              <ListItemText primary="Speed:" secondary={herois[0].powerstats.speed}></ListItemText>
+              <ListItemText primary="Slug:" secondary={heroi.slug}></ListItemText>
             </ListItem>
             <ListItem>
-              <ListItemText primary="Durability:" secondary={herois[0].powerstats.durability}></ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Power:" secondary={herois[0].powerstats.power}></ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Combat:" secondary={herois[0].powerstats.combat}></ListItemText>
+              <ListItemText primary="Powerstats:">
+              </ListItemText>          
+              <List component="div">
+                <ListItem>
+                  <ListItemText primary="Intelligence:" secondary={heroi.powerstats.intelligence}></ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Strength:" secondary={heroi.powerstats.strength}></ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Speed:" secondary={heroi.powerstats.speed}></ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Durability:" secondary={heroi.powerstats.durability}></ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Power:" secondary={heroi.powerstats.power}></ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Combat:" secondary={heroi.powerstats.combat}></ListItemText>
+                </ListItem>
+              </List>
             </ListItem>
           </List>
-        </ListItem>
-      </List>
+        ))
+      }
     </div>
   );
 }
